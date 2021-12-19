5067a547f10fd511f0cdc10e67862042ea620efa
AMBILI P A <ambilianilkumar857@gmail.com>
2021-12-19 21:12:59 +0530

    Home Controller have edited

--- HOSPITALMANAGEMENTSYSTEM/Controllers/HomePageController.cs
+++ HOSPITALMANAGEMENTSYSTEM/Controllers/HomePageController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using HOSPITALMANAGEMENTSYSTEM.Models;
 namespace HOSPITALMANAGEMENTSYSTEM.Controllers
 {
     public class HomePageController : Controller
@@ -17,19 +17,45 @@ namespace HOSPITALMANAGEMENTSYSTEM.Controllers
         {
             return View();
         }
+        
+        
         public ActionResult About()
         {
             return View();
         }
+        
         public ActionResult AdminLogin()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AdminLogin(Login l)
+        {
+            if (l.Username == "admin" && l.Password == "admin")
+            {
+                return RedirectToAction("AdminHome", "Admin");
+            }
+            else
+            {
+                ViewBag.info = "Please Check the credentials";
+            }
             return View();
         }
         public ActionResult DoctorLogin()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult DoctorLogin(Login l)
+        {
+            return View();
+        }
         public ActionResult PatientLogin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PatientLogin(Login l)
         {
             return View();
         }
